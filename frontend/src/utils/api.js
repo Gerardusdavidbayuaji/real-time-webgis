208266724803f0f7ae91660a4186b3495c3bfc95
Gerardusdavid <142146086+Gerardusdavidbayuaji@users.noreply.github.com>
2025-04-15 11:34:13 +0700

    feat: update api

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -19,3 +19,14 @@ export const getRealTimeData = async () => {
     throw Error("Upss, error fetching sensor data", error);
   }
 };
+
+export const getSemporLocation = async () => {
+  try {
+    const response = await axios.get(
+      "http://localhost:8080/geoserver/demo_serayu_opak/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=demo_serayu_opak%3Alokasi_bendungan_sempor&outputFormat=application%2Fjson"
+    );
+    return response.data;
+  } catch (error) {
+    throw Error("Upss, error fetching sempor location", error);
+  }
+};
---
+++ frontend/src/utils/mock.js
@@ -0,0 +1,14 @@
+export const damInformation = [
+  {
+    id: "1",
+    title: "Sempor Information",
+    description:
+      "Bendungan Sempor terletak di Desa Sempor, Kecamatan Sempor, Kabupaten Kebumen, Provinsi Jawa Tengah. Bendungan ini berada di wilayah sungai Serayu-Bogowonto dan termasuk dalam daerah aliran sungai Telomoyo. Dibangun sejak tahun 1958 hingga 1978, bendungan ini memiliki tinggi dasar sungai 49 meter, panjang puncak 220 meter, lebar puncak 10 meter, dan elevasi puncak mencapai 77 meter. Volume tampung normalnya sebesar 52.000.000 m³, sementara volume tampung total mencapai 47.000.000 m³.",
+  },
+  {
+    id: "2",
+    title: "Sempor History",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  },
+];
